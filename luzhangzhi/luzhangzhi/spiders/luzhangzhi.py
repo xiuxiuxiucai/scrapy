a835ab3c393e289b81a70970077e7fc69d9b960c
zhushuai <1342148984@qq.com>
2020-12-03 17:43:31 +0800

    fix: 修改路长制数据获取方式为正则表达式

--- luzhangzhi/luzhangzhi/spiders/luzhangzhi.py
+++ luzhangzhi/luzhangzhi/spiders/luzhangzhi.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import time
 import xlsxwriter
 import re
+from tqdm import tqdm
 
 
 # 搜索商品
@@ -55,25 +56,58 @@ def get_product(account, password):
     # 设置每页显示条数
     driver.find_element_by_xpath("//button[@class='btn btn-default dropdown-toggle']").click()
     page_num = driver.find_element_by_xpath("//ul[@class='dropdown-menu']/li[5]/a")
-    driver.execute_script("arguments[0].innerHTML=30", page_num)
+    driver.execute_script("arguments[0].innerHTML=200", page_num)
     time.sleep(1)
     # 查询
     print("正在查询...")
     page_num.click()
     time.sleep(3)
 
-    html = driver.page_source
-    data_text_one = re.findall(r"<tr data-index=\"1\">.*</tr>", html)
-    print("aaaaa")
-    print(data_text_one)
 
-    data_text_two = re.findall(r"<td style=\"text-align: center; vertical-align: middle; \">.*</td>", data_text_one[0])
-    print("bbbbb")
-    print(data_text_two)
+# 正则表达式爬取数据
+def re_parse_product():
+    # 获取总页数
+    page_total = int(driver.find_element_by_xpath("//li[@class='page-last']/a").text)
+    print("查询成功，数据总页数为：", page_total)
+    pbar = tqdm(total=page_total)
+
+    # 创建一个工作簿并添加一张工作表
+    workbook = xlsxwriter.Workbook("result.xlsx")
+    worksheet = workbook.add_worksheet()
+
+    worksheet.write(0, 0, "序号")
+    worksheet.write(0, 1, "站点名称")
+    worksheet.write(0, 2, "数据时间")
+    worksheet.write(0, 3, "AQI")
+    worksheet.write(0, 4, "PM10")
+    # 从第二行开始
+    row = 1
+
+    i = 0
+    while i < 3:
+        i += 1
+        pbar.update(1)
+        html = driver.page_source
+        data_text_one = re.findall(r"<tr data-index=\"0\">.*</tr>", html)
+        data_text_two = re.findall(r"(?<=<td style=\"text-align: center; vertical-align: middle; \">).*?(?=</td>)", data_text_one[0])
+
+        j = 0
+        for data_text in data_text_two:
+            worksheet.write(row, j, data_text)
+            j = j + 1
+            if j == 5:
+                row += 1
+                j = 0
+
+        # 下一页
+        driver.find_element_by_xpath("//li[@class='page-next']/a").click()
+        time.sleep(2)
+
+    workbook.close()
 
 
-# 爬取数据
-def parse_product():
+# css选择器爬取数据
+def css_parse_product():
     # 获取总页数
     page_total = int(driver.find_element_by_xpath("//li[@class='page-last']/a").text)
     print("数据总页数为：", page_total)
@@ -140,5 +174,6 @@ driver = webdriver.Chrome(options=chrome_options)
 print("正在打开网页...")
 driver.get("http://117.78.34.39:7078/DataQuery/StationHistoryData")
 driver.implicitly_wait(10)
+
 get_product(account, password)
-# parse_product()
+re_parse_product()
--- luzhangzhi/luzhangzhi/spiders/testTwo.py
+++ luzhangzhi/luzhangzhi/spiders/testTwo.py
@@ -1,7 +1,19 @@
-import re
+import time
+from tqdm import tqdm
 
-s = "are are smarter than dogs"
+# with tqdm(total=200) as pbar:
+#     pbar.set_description('Processing:')
+#     # total表示总的项目, 循环的次数20*10(每次更新数目) = 200(total)
+#     for i in range(20):
+#         # 进行动作, 这里是过0.1s
+#         time.sleep(0.1)
+#         # 进行进度更新, 这里设置10个
+#         pbar.update(10)
 
-result = re.findall("(are).*(than)", s)
-
-print(result)
+pbar = tqdm(total=100)
+print("111")
+pbar.update(10)
+print("111")
+pbar.update(10)
+print("222")
+pbar.update(10)
